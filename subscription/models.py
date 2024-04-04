b5dce2e666e9eb54a6f84cede0156d5f1f051733
Jinsun Yoo <jsyoo1029@gmail.com>
2024-04-04 22:32:50 +0900

    구독권 모델 코드
    Subscription, Payment

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    startDate = models.DateTimeField(auto_now_add=True)
+    endDate = models.DateField()
+    price = models.FloatField()
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    status = models.CharField(max_length=100)
+    paymentDate = models.DateField()
