4cc93eda5275316d204c1170389fe332b49d32ba
Jinsun Yoo <jsyoo1029@gmail.com>
2024-04-10 20:16:39 +0900

    subscription CRUD(viewset)

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Subscription"
