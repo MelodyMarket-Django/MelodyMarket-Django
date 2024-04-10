fb3f5e03dae826be35e651ce436bdf639ed6bf46
Sunha Shin <shinsunha11@gmail.com>
2024-04-10 03:03:10 -0400

    🔧 템플릿 파일 추가 및 모델 수정
    앨범 모델 삭제

--- playlist/models.py
+++ playlist/models.py
@@ -3,34 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Playlist(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="playlists")
-    name = models.CharField(max_length=255)
-    creation_date = models.DateField()
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    creation_date = models.DateField(auto_now_add=True)
 
 
 class Song(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    title = models.CharField(max_length=255)
-    artist = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
     duration = models.FloatField()
-    album = models.ForeignKey("Album", on_delete=models.CASCADE, related_name="songs")
 
 
 class PlaylistSong(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    playlist = models.ForeignKey(
-        Playlist, on_delete=models.CASCADE, related_name="playlist_songs"
-    )
-    song = models.ForeignKey(
-        Song, on_delete=models.CASCADE, related_name="playlist_songs"
-    )
-    order = models.IntegerField()
-
-
-class Album(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    name = models.CharField(max_length=255)
-    genre = models.ForeignKey("Genre", on_delete=models.CASCADE, related_name="albums")
-    release_date = models.DateField()
-    description = models.TextField()
+    id = models.AutoField(primary_key=True)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
--- playlist/views.py
+++ playlist/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .models import Playlist, Track
-from .serializers import PlaylistSerializer, TrackSerializer
+from .models import Playlist
+from .serializers import PlaylistSerializer
 
 
 class PlaylistViewSet(viewsets.ModelViewSet):
