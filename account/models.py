9fbc9c0940cfb3842fc2b68938303007f2d89bbb
YooJi3un <yooji3un@gmail.com>
2024-04-13 03:05:22 +0900

    feature-account--유저모델추가
    유저 모델 추가

--- account/models.py
+++ account/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    userID = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    gender = models.CharField(max_length=50, blank=True, null=True)
+    birthDate = models.DateField()
+    provider = models.CharField(max_length=255, blank=True, null=True)
+    accountId = models.CharField(max_length=255, blank=True, null=True)
+    isSubscribed = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+
+    def __str__(self):
+        return self.username
+
+
