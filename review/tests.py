5f95631e4de58fc56ce26190fbcee4ccc7c3c145
mingzinote <157416439+mingzinote@users.noreply.github.com>
2024-04-15 08:42:07 +0900

    Update tests.py

--- review/tests.py
+++ review/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Review, Song
 
-# Create your tests here.
+class ReviewModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # 테스트를 위한 사용자와 노래 생성
+        test_user = User.objects.create_user(username='testuser', password='12345')
+        test_song = Song.objects.create(title='Test Song', artist='Test Artist')
+        Review.objects.create(user=test_user, song=test_song, rating=5, comment='Great Song')
+
+    def test_review_content(self):
+        review = Review.objects.get(id=1)
+        expected_user = f'{review.user.username}'
+        expected_song = f'{review.song.title}'
+        expected_rating = review.rating
+        expected_comment = review.comment
+        self.assertEqual(expected_user, 'testuser')
+        self.assertEqual(expected_song, 'Test Song')
+        self.assertEqual(expected_rating, 5)
+        self.assertEqual(expected_comment, 'Great Song')
+
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Review, Song
+
+class ReviewModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # 테스트를 위한 사용자와 노래 생성
+        test_user = User.objects.create_user(username='testuser', password='12345')
+        test_song = Song.objects.create(title='Test Song', artist='Test Artist')
+        Review.objects.create(user=test_user, song=test_song, rating=5, comment='Great Song')
+
+    def test_review_content(self):
+        review = Review.objects.get(id=1)
+        expected_user = f'{review.user.username}'
+        expected_song = f'{review.song.title}'
+        expected_rating = review.rating
+        expected_comment = review.comment
+        self.assertEqual(expected_user, 'testuser')
+        self.assertEqual(expected_song, 'Test Song')
+        self.assertEqual(expected_rating, 5)
+        self.assertEqual(expected_comment, 'Great Song')
+
